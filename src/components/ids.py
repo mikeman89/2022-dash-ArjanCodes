0fb795825c9e19025008b482b2d548d2e58441f8
Michael Al Tork <michael.altork@gmail.com>
2023-01-07 13:39:04 -0500

    adding categories and months filters and dropdowns

--- src/components/ids.py
+++ src/components/ids.py
@@ -1,5 +1,8 @@
 NATION_DROPDOWN = "nation-dropdown"
 SELECT_ALL_YEARS_BUTTON = "select-all-years-button"
 YEAR_DROPDOWN = "year-dropdown"
-
+MONTH_DROPDOWN = "month-dropdown"
 BAR_CHART = "bar-chart"
+SELECT_ALL_MONTHS_BUTTON = "select-all-months-button"
+SELECT_ALL_CATEGORIES_BUTTON = "select-all-categories-button"
+CATEGORY_DROPDOWN = "category-dropdown"
