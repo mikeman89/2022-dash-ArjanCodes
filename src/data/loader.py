fbf277121d07cbe1202bd61c083c51b9d49bceab
Michael Al Tork <michael.altork@gmail.com>
2023-01-09 16:55:16 -0500

    add preprocessor to the loader file

--- src/data/loader.py
+++ src/data/loader.py
@@ -1,3 +1,6 @@
+from functools import reduce
+from typing import Callable
+
 import pandas as pd
 
 
@@ -9,14 +12,30 @@ class DataSchema:
     YEAR = "year"
 
 
+Preprocessor = Callable[[pd.DataFrame], pd.DataFrame]
+
+
+def create_year_column(df: pd.DataFrame) -> pd.DataFrame:
+    df_temp = df.copy()
+    df_temp[DataSchema.YEAR] = df_temp[DataSchema.DATE].dt.year.astype(str)
+    return df_temp
+
+
+def create_month_column(df: pd.DataFrame) -> pd.DataFrame:
+    df_temp = df.copy()
+    df_temp[DataSchema.MONTH] = df_temp[DataSchema.DATE].dt.month.astype(str)
+    return df_temp
+
+
+def compose(*functions: Preprocessor) -> Preprocessor:
+    return reduce(lambda f, g: lambda x: g(f(x)), functions)
+
+
 def load_transaction_data(path: str) -> pd.DataFrame:
     data = pd.read_csv(
         path,
         dtype={DataSchema.AMOUNT: float, DataSchema.CATEGORY: str},
         parse_dates=[DataSchema.DATE],
     )
-
-    data[DataSchema.MONTH] = data[DataSchema.DATE].dt.month.astype(str)
-    data[DataSchema.YEAR] = data[DataSchema.DATE].dt.year.astype(str)
-
-    return data
+    preprocessor = compose(create_year_column, create_month_column)
+    return preprocessor(data)
